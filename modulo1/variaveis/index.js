b6c6a5e99c11011311a8792619aaeeaedbb8f0ce
Ricardo Veronezi Ferrão <rvferrao@gmail.com>
2022-04-11 23:16:30 -0300

    Primeiros execícios de Variáveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,89 @@
+/*
+let a = 10
+let b = 10
+
+console.log(b)
+
+b = 5
+console.log(a, b)
+*/
+/*
+let a = 10
+let b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c)
+*/
+/*
+let horasPorDia = prompt("Quantas horas você trabalha por dia?")
+let salarioPorDia = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${salarioPorDia/horasPorDia} por hora`)
+*/
+
+//EXERCÍCIO 1
+
+//a) declare variavel nome
+//let nome
+
+//b) declare variavel idade
+//let idade
+
+//c) imprima o tipo das variaveis
+//console.log(typeof nome)
+//console.log(typeof idade)
+
+//d) deu valor undefined porque a variável não teve o tipo declarado
+
+//e) Pergunte seu nome e sua idade
+//const nome = prompt("Qual o seu nome?")
+//const idade = prompt("Qual a sua idade?")
+//console.log("Seu nome é:", nome)
+//console.log("Sua idade é:", idade)
+
+//f) imprima novamente o tipo das variaveis
+//console.log(typeof nome)
+//console.log(typeof idade)
+//o tipo das variaveis mudou para string
+
+//g) Imprimir frase
+//console.log("Olá ", "Seu nome é ", nome, "voce tem ", idade, "anos")
+
+//EXERCICIO 2
+/*
+let respostaUm = true
+let respostaDois = true
+let respostaTres = true
+
+ respostaUm = prompt("Voce tem carro?")
+ respostaDois = prompt("Voce esta comendo milho?")
+ respostaTres = prompt("Voce sabe voar?")
+*/
+
+//EXERCICIO 3
+/*
+let a = 10
+let b = 25
+
+console.log("O valor de a é", a) // O valor de a é 25
+console.log("O valor de b é", b) // O valor de b é 10
+
+c = a
+a = b
+b = c
+
+console.log("O novo valor de a é", a) // O novo valor de a é 25
+console.log("O novo valor de b é", b) // O novo valor de b é 10
+*/
+
+
+
+
+
+
+
+
+
+
+
