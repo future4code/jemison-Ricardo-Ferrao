565e4cbf6094c4093df00be58e0635165229efb6
Ricardo Veronezi Ferrão <rvferrao@gmail.com>
2022-05-10 21:02:08 -0300

    Revisão-JS - Primeiro envio

---
+++ modulo1/revisao-js/exercicios.js
@@ -0,0 +1,116 @@
+// ATENÇÃO!!!
+//    -> NÃO COMENTE NENHUMA DAS FUNÇÕES DECLARADAS!!! 
+//    -> NÃO MODIFIQUE OS PARÂMETROS DAS FUNÇÕES!!! ()
+
+
+// EXERCÍCIO 01
+function retornaTamanhoArray(array) {
+   return array.length
+ 
+}
+
+
+// EXERCÍCIO 02
+function retornaArrayInvertido(array) {
+    return array.reverse(array)
+  
+
+}
+
+// EXERCÍCIO 03
+function retornaArrayOrdenado(array) {
+    return array.sort(function(a, b){ return a > b; })
+  
+}
+
+// EXERCÍCIO 04
+function retornaNumerosPares(array) {
+
+    for(let i=0; i < array.length; i++){
+
+        if(array[i] % 2 == 0){
+
+        let par = [array[i]]
+        }
+        
+    }
+return par
+
+}
+
+// EXERCÍCIO 05
+function retornaNumerosParesElevadosADois(array) {
+ 
+}
+
+// EXERCÍCIO 06
+function retornaMaiorNumero(array) {
+
+    let maiorNumero
+
+    return maiorNumero
+}
+
+
+// EXERCÍCIO 07
+function retornaObjetoEntreDoisNumeros(num1, num2) {
+    
+    if (num1 > num2){
+        maiorNumero = num1
+    }else {(num2 > num1)
+        maiorNumero = num2
+    return maiorNumero
+    }
+    
+
+}
+
+// EXERCÍCIO 08
+function retornaNPrimeirosPares(n) {
+   
+}
+
+// EXERCÍCIO 09
+function classificaTriangulo(ladoA, ladoB, ladoC) {
+
+}
+
+// EXERCÍCIO 10
+function retornaSegundoMaiorESegundoMenor(array) {
+  
+}
+
+// EXERCÍCIO 11
+function retornaChamadaDeFilme(filme) {
+   
+}
+
+// EXERCÍCIO 12
+function retornaPessoaAnonimizada(pessoa) {
+   
+}
+
+// EXERCÍCIO 13A
+function retornaPessoasAutorizadas(pessoas) {
+   
+}
+
+// EXERCÍCIO 13B
+function retornaPessoasNaoAutorizadas(pessoas) {
+  
+}
+
+// EXERCÍCIO 14
+function retornaContasComSaldoAtualizado(contas) {
+
+}
+
+// EXERCÍCIO 15A
+function retornaArrayOrdenadoAlfabeticamente(consultas) {
+  
+}
+
+// EXERCÍCIO 15B
+function retornaArrayOrdenadoPorData(consultas) {
+   
+}
