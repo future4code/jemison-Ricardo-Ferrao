e3db55888a27a3052f648cf9787242be1dc5b7ec
Ricardo Veronezi Ferrão <rvferrao@gmail.com>
2022-05-12 16:46:12 -0300

    revisao-js

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -25,30 +25,41 @@ function retornaArrayOrdenado(array) {
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
+    let arrayPar = []
 
     for(let i=0; i < array.length; i++){
 
         if(array[i] % 2 == 0){
 
-        let par = [array[i]]
+        arrayPar.push(array[i])
         }
         
     }
-return par
+return arrayPar
 
 }
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
- 
+    const numerosPares = array.filter((item) => {
+        return item % 2 === 0
+    })
+    const resultado = numerosPares.map((item2) => {
+        return item2 * item2
+    })
+ return resultado
+
 }
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-
-    let maiorNumero
-
-    return maiorNumero
+    let maiorNumero = 0
+    for(let i = 0; i < array.length; i++) {
+      if(array[i] > maiorNumero) {
+        maiorNumero = array[i]
+      }
+    }
+  return maiorNumero
 }
 
 
@@ -61,7 +72,7 @@ function retornaObjetoEntreDoisNumeros(num1, num2) {
         maiorNumero = num2
     return maiorNumero
     }
-    
+
 
 }
 
@@ -72,7 +83,14 @@ function retornaNPrimeirosPares(n) {
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
-
+    if(ladoA === ladoB && ladoB === ladoC && ladoC === ladoA) {
+        return 'Equilátero'
+    }else if (ladoA === ladoB && ladoA !== ladoC) {
+        return 'Isósceles'
+    }else if (ladoA !== ladoB && ladoB !== ladoC && ladoC !== ladoA){
+        return 'Escaleno'
+    
+    }
 }
 
 // EXERCÍCIO 10
@@ -92,25 +110,44 @@ function retornaPessoaAnonimizada(pessoa) {
 
 // EXERCÍCIO 13A
 function retornaPessoasAutorizadas(pessoas) {
-   
+    return pessoas.filter((item) => {
+        return (item.idade > 14 && item.idade < 60 && item.altura > 1.5)
+    })
 }
 
 // EXERCÍCIO 13B
 function retornaPessoasNaoAutorizadas(pessoas) {
+    return pessoas.filter((item) => {
+        return (item.idade <= 14 || item.idade > 60 || item.altura < 1.5)
+    })
   
 }
 
 // EXERCÍCIO 14
 function retornaContasComSaldoAtualizado(contas) {
-
+    let pago = {
+        ...contas,
+        compras: Reduce(function(soma, i) {return soma + i})
+    }
+    let total = {
+        ...pago,
+        saldoTotal: total - compras
+    }
+    return saldoTotal
 }
 
 // EXERCÍCIO 15A
 function retornaArrayOrdenadoAlfabeticamente(consultas) {
-  
+    consultas.sort((a,b) => {
+        if (a.nome < b.nome){
+            return -1
+        }else {return true}
+    })
+  return consultas
 }
 
 // EXERCÍCIO 15B
 function retornaArrayOrdenadoPorData(consultas) {
+
    
 }
