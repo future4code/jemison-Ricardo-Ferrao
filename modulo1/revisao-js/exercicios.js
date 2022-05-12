cf3887170edc112cf6fedec426a85bdc28f1a8ff
Ricardo Veronezi Ferrão <rvferrao@gmail.com>
2022-05-12 20:58:21 -0300

    revisao-js

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -19,7 +19,9 @@ function retornaArrayInvertido(array) {
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
-    return array.sort(function(a, b){ return a > b; })
+    return array.sort(function(a, b){ 
+        return a > b 
+    })
   
 }
 
@@ -65,27 +67,27 @@ function retornaMaiorNumero(array) {
 
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
-    
-    if (num1 > num2){
-        maiorNumero = num1
-    }else {(num2 > num1)
-        maiorNumero = num2
-    return maiorNumero
-    }
-
 
 }
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
-   
+  let i = 0
+  let arrayResposta = []
+  while(i < n){
+    arrayResposta.push(i*2)
+    i++
+  }
+  return arrayResposta
 }
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
     if(ladoA === ladoB && ladoB === ladoC && ladoC === ladoA) {
         return 'Equilátero'
-    }else if (ladoA === ladoB && ladoA !== ladoC) {
+    }else if ((ladoA === ladoB && ladoA !== ladoC) 
+    || (ladoA === ladoC && ladoA !== ladoB) 
+    || (ladoA !== ladoB && ladoB === ladoC)) {
         return 'Isósceles'
     }else if (ladoA !== ladoB && ladoB !== ladoC && ladoC !== ladoA){
         return 'Escaleno'
@@ -95,23 +97,45 @@ function classificaTriangulo(ladoA, ladoB, ladoC) {
 
 // EXERCÍCIO 10
 function retornaSegundoMaiorESegundoMenor(array) {
-  
+
+
 }
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
+    const filmeChamada = {
+        nome: "O Diabo Veste Prada",
+        ano: 2006,
+        diretor: "David Frankel",
+        atores: ["Meryl Streep", "Anne Hathaway", "Emily Blunt", "Stanley Tucci"],
+    }
+     
+    return `Venha assistir ao filme ${filmeChamada.nome}, de ${filmeChamada.ano}, dirigido por ${filmeChamada.diretor} e estrelado por ${filmeChamada.atores[0]}, ${filmeChamada.atores[1]}, ${filmeChamada.atores[2]}, ${filmeChamada.atores[3]}.`
+    
    
 }
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   
+    const pessoa1 = {
+        nome: "Astrodev",
+        idade: 25,
+        email: "astrodev@labenu.com.br",
+        endereco: "Rua do Futuro, 4"
+    }
+    const anonimo = {
+        ...pessoa,
+        nome: "ANÔNIMO",
+    }
+      return anonimo
 }
 
 // EXERCÍCIO 13A
 function retornaPessoasAutorizadas(pessoas) {
     return pessoas.filter((item) => {
-        return (item.idade > 14 && item.idade < 60 && item.altura > 1.5)
+        return (item.idade > 14 
+            && item.idade < 60 
+            && item.altura > 1.5)
     })
 }
 
@@ -125,15 +149,7 @@ function retornaPessoasNaoAutorizadas(pessoas) {
 
 // EXERCÍCIO 14
 function retornaContasComSaldoAtualizado(contas) {
-    let pago = {
-        ...contas,
-        compras: Reduce(function(soma, i) {return soma + i})
-    }
-    let total = {
-        ...pago,
-        saldoTotal: total - compras
-    }
-    return saldoTotal
+
 }
 
 // EXERCÍCIO 15A
@@ -148,6 +164,10 @@ function retornaArrayOrdenadoAlfabeticamente(consultas) {
 
 // EXERCÍCIO 15B
 function retornaArrayOrdenadoPorData(consultas) {
-
-   
+    consultarData.sort((a,b) => {
+        if (a.dataDaConsulta < b.dataDaConsulta){
+            return-1
+        }else {return true}
+    })
+    return consultarData
 }
