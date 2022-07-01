e2ccf565ec1528df318322f6596d130122bc3483
Ricardo Veronezi Ferrão <rvferrao@gmail.com>
2022-07-01 19:52:22 -0300

    aula-styled-components

--- modulo3/aula-styled-components/ex-styled-components/src/App.js
+++ modulo3/aula-styled-components/ex-styled-components/src/App.js
@@ -1,23 +1,70 @@
+import React from 'react';
 import logo from './img/LabZap.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const Container = styled.main`
+  
+  height: 90vw;
+  background-color: white;
+  display: flex;
+  justify-content: space-between;
+
+`
+
+const SpanEsquerdo = styled.span`
+  background-color: orange;
+  padding: 8vw;
+
+`
+const SpanDireito = styled.span`
+  background-color: orange;
+  padding: 8vw;
+`
+
+const Header = styled.header`
+  text-align: center;  
+`
+const Footer = styled.footer`
+  background-color: grey;
+  text-align: center;
+  font-size:  1em;
+`
+
+const Form = styled.form`
+  text-align: center;
+  justify-content: space-around;
+`
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header>
+        <img src={logo} ClassName="App-logo" alt="logo" />
+      </Header>
+
+
+      <Container>
+        <SpanEsquerdo>
+        </SpanEsquerdo>
+        <SpanDireito>
+        </SpanDireito>
+      </Container>
+      <Form>
+        <div>
+          <label for="nome">Remetente:</label>
+          <input type="text" id="nome" />
+
+          <label for="msg">Mensagem:</label>
+          <input type="text" id="mensagem" />
+	        <button type="submit">Enviar Mensagens</button>  
+        </div>
+      </Form>
+      
+      <Footer>
+        Copyright Ricardo Veronezi Ferrão All rights reserved
+      </Footer>
     </div>
   );
 }
