ba7db0a15d93406437bb863a05a124dea599abf8
Ricardo Veronezi Ferrão <rvferrao@gmail.com>
2022-07-07 19:39:16 -0300

    estados-insta4-listas2

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -1,18 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components'
 import Post from './components/Post/Post';
 
-const MainContainer = styled.div`
+const Card = styled.section`
+  display: flex;
+  justify-content: space-between;
+  width: 50%;
+  margin: 10px auto;
+  padding: 10px;
+  border: 1px solid #000000;
+  border-radius: 10px;
+
+  .coral {
+    color: coral;
+    font-weight: bold;
+  }
+
+  :hover {
+    cursor: pointer;
+    background-color: lightsalmon;
+  }
+`
+const Form = styled.form`
   display: flex;
   justify-content: center;
-  flex-direction: column;
   align-items: center;
+
+  label {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip: rect(0,0,0,0);
+    border: 0;
+  }
+
+  input {
+    padding: 10px 5px;
+    border: 1px solid black;
+    border-radius: 10px;
+    margin: 10px;
+    width: 20%;
+  }
+
+  button {
+    border: none;
+    border-radius: 10px;
+    padding: 10px;
+    background-color: lightsalmon;
+  }
+
+  button:hover {
+    cursor: pointer;
+    color: white;
+  }
 `
-//Usando ARRAY
 
 function App() {
+  const [inputNome, setInputNome] = useState("")
+  const [inputIdade, setInputIdade] = useState("")
+  const [pessoas, setPessoas] = useState([
   
-  const Pessoas = [
+//  const Pessoas = [
     {
       nomeUsuario:'paulinha',
       fotoUsuario: 'https://picsum.photos/50/50',
@@ -31,51 +82,65 @@ function App() {
       fotoPost:'https://picsum.photos/300/250'
     }
   ]
+  )
+  const handleInputNome = (e) => {
+    setInputNome(e.target.value)
+  }
 
-  const novoArray = Pessoas.map((item, index) =>{
+  const handleInputIdade = (e) => {
+    setInputIdade(e.target.value)
+  }
+
+  const addPessoa = (e) => {
+    e.preventDefault();
+
+    const novaPessoa = {nome: inputNome, idade: inputIdade}
+    const novaListaDePessoas = [...pessoas, novaPessoa]
+    setPessoas(novaListaDePessoas)
+  }
+                     
+  const listaDePessoas = pessoas.map((pessoa, index) => {
+  
+    const deletarPessoa = () => {
+      const novaListaDePessoas = [...pessoas]
+      const deletar = novaListaDePessoas.findIndex((pessoaExcluida) => {
+        return pessoaExcluida === pessoa
+      })
+      novaListaDePessoas.splice(deletar, 1)
+      setPessoas(novaListaDePessoas)
+    }
+
     return (
-      <Post key={index}
-        nomeUsuario = {item.nomeUsuario}
-        fotoUsuario = {item.fotoUsuario}
-        fotoPost = {item.fotoPost}
-      />
+      <Card key={index}>
+        <p>{pessoa.nome}</p> 
+        <p className='coral'>{pessoa.idade}</p>
+        <button onClick={deletarPessoa}>X</button>
+      </Card>
     )
   })
-  console.log(novoArray)
 
-  return(
-    <MainContainer>
-      {novoArray}
-    </MainContainer>
+  return (
+    <main>
+      <Form>
+        <label>Nome:</label>
+        <input
+          placeholder='Insira um nome'
+          value={inputNome}
+          onChange={handleInputNome}
+        />
+        
+        <label>Idade:</label>
+        <input
+          placeholder='Insira uma idade'
+          value={inputIdade}
+          onChange={handleInputIdade}
+        />
+        <button onClick={addPessoa}>Adicionar</button>
+      </Form>
+      {listaDePessoas}
+    </main>
   )
 }
 
-
-
-/*
-function App() {
-return(
-  <MainContainer>
-           <Post
-            nomeUsuario={'paulinha'}
-            fotoUsuario={'https://picsum.photos/50/50'}
-            fotoPost={'https://picsum.photos/200/150'}
-          />
-           <Post
-            nomeUsuario={'jaime'}
-            fotoUsuario={'https://picsum.photos/52/52'}
-            fotoPost={'https://picsum.photos/100/50'}
-          />
-           <Post
-            nomeUsuario={'Lucas'}
-            fotoUsuario={'https://picsum.photos/54/54'}
-            fotoPost={'https://picsum.photos/300/250'}
-          />
-        </MainContainer>
-        
-)
-
-}
-*/
-
 export default App;
+
