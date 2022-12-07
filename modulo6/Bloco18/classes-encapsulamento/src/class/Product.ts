59a2fc8dd49c5e87f0c3c3168705fcb57aae2af9
Ricardo Veronezi Ferrão <rvferrao@gmail.com>
2022-12-06 21:36:37 -0300

    Exercicios 1 e 2

---
+++ modulo6/Bloco18/classes-encapsulamento/src/class/Product.ts
@@ -0,0 +1,13 @@
+export class Product{
+    private id: string
+    private name: string
+    private price: number
+
+    constructor ( id: string, name: string, price: number){
+        this.id=id
+        this.name = name
+        this.price = price
+    }
+}
+
+
